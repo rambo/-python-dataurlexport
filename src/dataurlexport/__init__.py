8364f23f1935a08520fcbd8f24dd6b5a65e797cc
Eero af Heurlin <eero.afheurlin@iki.fi>
2025-09-07 02:07:53 +0300

    chore: bump version

--- src/dataurlexport/__init__.py
+++ src/dataurlexport/__init__.py
@@ -1,3 +1,3 @@
 """Post-process RUNE outputs and export data urls as files"""
 
-__version__ = "0.1.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "0.2.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
--- tests/test_dataurlexport.py
+++ tests/test_dataurlexport.py
@@ -5,4 +5,4 @@ from dataurlexport import __version__
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.2.0"
